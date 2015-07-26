dcc62fecd005d2cbe66db832dbbe53e5afc7c182
Alex Dijkstra <alex@loginet.nl>
2015-07-26 20:58:28 +0200

    Create test.py

---
+++ project_name/settings/test.py
@@ -0,0 +1,21 @@
+"""
+This is an example settings/test.py file.
+Use this settings file when running tests.
+These settings overrides what's in settings/base.py
+"""
+
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = '{{ secret_key }}'
