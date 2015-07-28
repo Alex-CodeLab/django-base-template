0058cf4a98d90dec48ec0791181be2edd45270a5
allox <alex.sd3@gmail.com>
2015-07-28 13:01:57 +0200

    installer added

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Install Django 1.8"
+pip install "Django>=1.8"
+
+echo "Startproject"
+bin/django-admin.py startproject --template https://github.com/allox/django-base-template-1.8/zipball/master --extension py,md,rst mainapp && \
+
+mv mainapp/ src && \
+
+echo "Install requirements" && \
+cd src && \
+pip install -r requirements/local.txt && \
+cp mainapp/settings/local-dist.py mainapp/settings/local.py && \
+
+echo "Install completed. Create Database" && \
+chmod +x manage.py  && \
+./manage.py syncdb  && \
+./manage.py migrate && \
+./manage.py collectstatic && \
+./manage.py runserver
