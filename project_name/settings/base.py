8520157045746a4436f8730921083546875cc734
Alex <allox@users.noreply.github.com>
2016-07-10 12:19:13 +0200

    Update base.py

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -260,11 +260,11 @@ MANAGERS = ADMINS
 # SECURITY WARNING: don't run with debug turned on in production!
 # Debugging displays nice error messages, but leaks memory. Set this to False
 # on all server instances and True only for development.
-DEBUG = False
+DEBUG = TEMPLATE_DEBUG = True
 
 # Is this a development instance? Set this to True on development/master
 # instances and False on stage/prod.
-DEV = False
+DEV = True
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.8/ref/settings/#allowed-hosts
