43c0feb8500cbb22bf0b7c1ef14f8aeeb60ef12e
Alex Dijkstra <allox@users.noreply.github.com>
2016-09-05 20:54:48 +0200

    1.10

--- base/views.py
+++ base/views.py
@@ -1,12 +1,10 @@
 """ Views for the base application """
 
-from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+from django.shortcuts import render, render_to_response, HttpResponseRedirect
 
 def home(request):
     """ Default view for the root """
     return render(request, 'base/home.html')
     
 def profile(request):
-    return  render_to_response("user/profile.html",
-                               locals(),
-                               context_instance  = RequestContext(request))    
+    return  render(request, "user/profile.html" )    
