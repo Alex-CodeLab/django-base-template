60dd857503a882962d4956f1f683208a48e07cd8
allox <alex.sd3@gmail.com>
2016-10-13 17:09:41 +0200

    making base view great again

--- base/views.py
+++ base/views.py
@@ -8,8 +8,5 @@ def home(request):
     djangoversion = django.get_version()
     return render(request, 'base/home.html',{'djangoversion':djangoversion })
 
-
-return render(request, 'base/home.html')
-    
 def profile(request):
     return  render(request, "user/profile.html" )    
