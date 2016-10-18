56bfea91292a2f1670b47c8e6f4bdbb1034e6121
Alex Dijkstra <allox@users.noreply.github.com>
2016-10-18 12:43:08 +0200

    Update urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -29,3 +29,8 @@ urlpatterns = [
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
     urlpatterns += [ url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+
+    
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [ url(r'^__debug__/', include(debug_toolbar.urls)),]
