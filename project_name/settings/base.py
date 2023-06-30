f91bb1a3344ea7ffdeb36b95df1f511904ff2b84
Alex <1678423+Alex-CodeLab@users.noreply.github.com>
2023-06-30 14:58:29 +0200

    Update base.py

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -185,6 +185,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
